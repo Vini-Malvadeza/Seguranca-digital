c2740c5aea75b476dfeab64b1678a310015c4983
Vini-Malvadeza <vinicius.pinheiro18@escola.pr.gov.br>
2026-07-08 23:28:19 +0000

    aula 5

--- main.js
+++ main.js
@@ -13,14 +13,30 @@ function diminuiTamanho(){
         tamanhoSenha--;
     }
     numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
 }
-
 function aumentaTamanho(){
     if (tamanhoSenha < 20){
        // tamanhoSenha = tamanhoSenha+1;
        tamanhoSenha++;
     }
     numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
 }
 
-console.log(botoes)
+const campoSenha = document.querySelector('#campo-senha');
+
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVXYWZ';
+geraSenha();
+
+function geraSenha(){
+    let senha = '' ”;
+    for (let i = 0; i < tamanhoSenha;i++){
+        let numeroAleatorio = Math.random()*letrasMaiusculas.length;
+        numeroAleatorio = Math.floor(numeroAleatorio);
+        senha = senha + letrasMaiusculas[numeroAleatorio];
+    }
+    campoSenha.value = senha;
+}
+
+campoSenha.value = letrasMaiusculas;
